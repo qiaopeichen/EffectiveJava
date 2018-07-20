4ed5a5a8786ab4432429efba5b2220b4f5b43d21
qiaopeichen <908694753@qq.com>
2018-07-20 20:19:18 +0800

    29.优先考虑类型安全的异构容器

--- 29.优先考虑类型安全的异构容器.java
+++ 29.优先考虑类型安全的异构容器.java
@@ -20,5 +20,19 @@ public static void main(String[] args) {
 
 // Typesafe heterogeneous container pattern - implementation
 public class Favorites {
-	private Map<Class<?>
+	private Map<Class<?>， Object> favorites = new HashMap<>();
+	public <T> void putFavorite(Class<T> type, T instance) {
+		if (type == null) {
+			throw new NullPointerException("Type is null");
+		}
+		favorites.put(type, instance);
+	}
+
+	public <T> T getFavorite(Class<T> type) {
+		return type.cast(favorites.get(type));
+	}
+}
+
+public class Class<T> {
+	T cast(Object obj);
 }
